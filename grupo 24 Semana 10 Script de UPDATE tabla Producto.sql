e3235716a64b0d91128ff290edb9d8a2b5f8b3c9
alenovasv <116722421+alenovasv@users.noreply.github.com>
2025-09-24 22:16:01 -0600

    Add files via upload

---
+++ grupo 24 Semana 10 Script de UPDATE tabla Producto.sql
@@ -0,0 +1,155 @@
+
+
+UPDATE PRODUCTO 
+
+SET nombre = "Sorbete de Mango" , descripcion = "Sorbete sabor mango verde" , precio_venta = 2.00 , STOCK = 125  
+
+Where id_producto = 1 ;  
+
+
+
+UPDATE PRODUCTO 
+
+SET nombre = "Sorbete de Sandia" , descripcion = "Sorbete de leche sabor sandia" , precio_original = 1.80 , stock = 50  
+
+Where id_producto = 2;  
+
+
+UPDATE PRODUCTO 
+
+SET stock = 120  
+
+Where id_producto = 3 ;  
+
+
+
+UPDATE PRODUCTO 
+
+SET precio_venta = 2.75 , stock = 60                                                                                                Where id_producto = 4 ;  
+ 
+
+UPDATE PRODUCTO  
+
+SET nombre = "Sorbete de coco light" , descripcion = "Sorbete sabor coco con leche deslactosada" , precio_venta = 2.00 , STOCK = 96  
+
+Where id_producto = 5 ; 
+
+
+
+UPDATE PRODUCTO  
+
+SET stock = 30  
+
+Where id_producto = 6 ; 
+
+ 
+
+
+
+UPDATE PRODUCTO  
+
+SET id_categoria = 3  
+
+Where id_producto = 7 ; 
+
+ 
+
+
+
+UPDATE PRODUCTO  
+
+SET nombre = "Sorbete familiar jumbo" , descripcion = "Presentacion para una familia de 4 personas" , precio_venta = 8.00 , STOCK = 50  
+
+Where id_producto = 8 ; 
+
+
+
+UPDATE PRODUCTO  
+
+SET nombre = "Sorbete familiar (2.5 litros)" , descripcion = "Sorbete en presentacion de 2.5 litros", precio_venta = 4.50 and stock = 70  
+
+Where id_producto = 9 ; 
+
+
+
+UPDATE PRODUCTO  
+
+SET nombre = "Mangoneada de jocote" , descripcion = "Helado tipo mangoneada con sabor a jocote" , precio_venta = 5.25, stock = 65  
+
+Where id_producto = 10 ; 
+
+
+
+UPDATE PRODUCTO SET Stock = 12 Where id_producto = 11 ; 
+
+UPDATE PRODUCTO SET nombre = "Sundae chocolate cacao" ,descripcion = "Helado de chocolate a base de cacao" , precio_venta = 3.15 , stock = 47  
+
+Where id_producto = 12 ; 
+
+ 
+
+
+
+UPDATE PRODUCTO  
+
+SET nombre = "Batido con helado de vainilla a base de leche deslactosada", descripcion = " Batido de vainilla fabricado con leche deslactosada", precio_original = 2.50 , precio_venta = 2.50 , stock = 35  
+
+Where id_producto = 13 ; 
+
+ 
+
+
+
+UPDATE PRODUCTO  
+
+SET nombre = "Batido con helado de chocolate Marca Hershey" , descripcion= "Batido con helado de chocolate de marca Hershey" , precio_venta = 6.25 , stock = 20  
+
+Where id_producto = 14 ; 
+
+
+
+UPDATE PRODUCTO  
+
+SET nombre = "Malteada de Chocolate blanco con Galleta" , descripcion="Batido con helado de chocolate blanco con trozos de galleta " , precio_venta = 3.50, stock = 20 Where id_producto = 15; 
+
+
+
+UPDATE PRODUCTO  
+
+SET nombre = "Paleta de arrayan" , descripcion = "Paleta helada artesanal de Arrayan" , precio_venta = 1.50 , stock = 36  
+
+Where id_producto = 16; 
+
+ 
+
+
+
+UPDATE PRODUCTO  
+
+SET nombre = "Paleta de frambuesa", descripcion = "Paleta helada artesanal de frambuesa" , precio_venta = 1.65, stock = 17  
+
+Where id_producto = 17; 
+
+
+
+UPDATE PRODUCTO 
+
+ SET nombre = "Paleta de tamarindo con chile", descripcion = "Paleta de tamarindo con chile tajin" , precio_venta = 1.25, stock = 150 
+
+ Where id_producto = 18; 
+
+
+
+UPDATE PRODUCTO  
+
+SET nombre = "Brownie con helado de leche condensada", descripcion = "Brownie con helado de leche condensada" , precio_venta = 3.75 , stock = 30  
+
+Where id_producto = 19; 
+
+
+
+UPDATE PRODUCTO  
+
+SET nombre = "Banana con chocolate y pixies" , descripcion = "Chocobanano con dulces pixies" , precio_venta = 0.75 , stock = 300  
+
+Where id_producto = 20; 
